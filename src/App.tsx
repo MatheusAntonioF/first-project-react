029718e61f852551a87a8516de2476e5d8d4e7ed
Matheus Antonio <matheusantoniof@gmail.com>
2020-09-10 11:51:45 -0300

    feat: create GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global';
+
 import Routes from './routes';
 
-const App: React.FC = () => <Routes />;
+const App: React.FC = () => (
+  <>
+    <GlobalStyles />
+    <Routes />
+  </>
+);
 
 export default App;
